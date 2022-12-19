c17d7117d3cee914415ff8711a80bb1f7cf5d41e
DavidSilveira80 <davidpscontato@gmail.com>
2022-12-19 11:18:19 -0300

    Adicionado campos da classe Bicicleta.

--- src/Bicicleta.java
+++ src/Bicicleta.java
@@ -1,7 +1,14 @@
+import java.time.LocalDate;
+
 public class Bicicleta {
+    Integer id;
+
     String modelo;
+    int aro;
+    String cor;
     String nomeDoComprador;
     String nomeDaLoja;
     double preco;
+    LocalDate dataDeCompra;
 
 }
