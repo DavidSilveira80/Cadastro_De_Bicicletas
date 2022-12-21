30cc6eb2216b4167ecdf60ca8357500b0cd000b6
DavidSilveira80 <davidpscontato@gmail.com>
2022-12-21 08:12:09 -0300

    Adicionado getters e setters e toString.

--- src/Bicicleta.java
+++ src/Bicicleta.java
@@ -24,4 +24,81 @@ public class Bicicleta {
         this.dataDeCompra = dataDeCompra;
     }
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAro() {
+        return aro;
+    }
+
+    public void setAro(int aro) {
+        this.aro = aro;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getNomeDoComprador() {
+        return nomeDoComprador;
+    }
+
+    public void setNomeDoComprador(String nomeDoComprador) {
+        this.nomeDoComprador = nomeDoComprador;
+    }
+
+    public String getNomeDaLoja() {
+        return nomeDaLoja;
+    }
+
+    public void setNomeDaLoja(String nomeDaLoja) {
+        this.nomeDaLoja = nomeDaLoja;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public LocalDate getDataDeCompra() {
+        return dataDeCompra;
+    }
+
+    public void setDataDeCompra(LocalDate dataDeCompra) {
+        this.dataDeCompra = dataDeCompra;
+    }
+
+    @Override
+    public String toString() {
+        return "Bicicleta{" +
+                "id=" + id +
+                ", modelo='" + modelo + '\'' +
+                ", aro=" + aro +
+                ", cor='" + cor + '\'' +
+                ", nomeDoComprador='" + nomeDoComprador + '\'' +
+                ", nomeDaLoja='" + nomeDaLoja + '\'' +
+                ", preco=" + preco +
+                ", dataDeCompra=" + dataDeCompra +
+                '}';
+    }
 }
