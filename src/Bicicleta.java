5120148afda840b1a6d9c70a2350c978cc4974f2
DavidSilveira80 <davidpscontato@gmail.com>
2022-12-20 22:58:11 -0300

    Adicionado construtor, internet falhando.

--- src/Bicicleta.java
+++ src/Bicicleta.java
@@ -11,4 +11,17 @@ public class Bicicleta {
     double preco;
     LocalDate dataDeCompra;
 
+    public Bicicleta(String modelo, int aro, String cor, double preco, String nomeDoComprador, String nomeDaLoja, LocalDate
+            dataDeCompra){
+
+        this.id = geradorId.contador();
+        this.modelo = modelo;
+        this.aro = aro;
+        this.cor = cor;
+        this.preco = preco;
+        this.nomeDoComprador = nomeDoComprador;
+        this.nomeDaLoja = nomeDaLoja;
+        this.dataDeCompra = dataDeCompra;
+    }
+
 }
