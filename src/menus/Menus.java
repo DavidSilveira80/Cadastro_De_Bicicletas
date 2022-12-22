d68d2dd900878baebe163da59b7c68e6fe5da44e
DavidSilveira80 <davidpscontato@gmail.com>
2022-12-22 17:54:24 -0300

    Adicionado diretório e classe Menus.

---
+++ src/menus/Menus.java
@@ -0,0 +1,15 @@
+package menus;
+
+public class Menus {
+    public static void menuPrincipal(){
+        System.out.println("===============================================================");
+        System.out.println("    1  ->   Criar Bike                                         ");
+        System.out.println("    2  ->   Listar todas as Bikes                              ");
+        System.out.println("    3  ->   Listar uma Bike por ID                             ");
+        System.out.println("    4  ->   Atualizar todas as informações da bike             ");
+        System.out.println("    5  ->   Atualizar alguma informação especifica de uma bike ");
+        System.out.println("    6  ->   Deletar uma Bike                                   ");
+        System.out.println("    7  ->   Sair                                               ");
+
+    }
+}
