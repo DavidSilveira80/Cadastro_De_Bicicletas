ad6dff3dd0812be1f7fc19965a5727e1865471e6
DavidSilveira80 <davidpscontato@gmail.com>
2022-12-27 09:29:56 -0300

    Conceito inicial do fluxo da aplicação principal.

--- src/Aplicacao.java
+++ src/Aplicacao.java
@@ -4,13 +4,34 @@ public class Aplicacao {
     public static void main(String[] args){
         Scanner entrada = new Scanner(System.in);
 
-        int option;
-        do{
-            Menus.menuPrincipal();
-            System.out.print("Informe sua escolha: ");
-            option = entrada.nextInt();
-        }while(option != 1 && option != 2 && option != 3 &&
-        option != 4 && option != 5 && option != 6 && option !=7);
+        int parada = 0;
+        while(parada == 0) {
+            int option;
+            do {
 
+                Menus.menuPrincipal();
+                System.out.print("Informe sua escolha: ");
+                option = entrada.nextInt();
+
+                if (option == 1) {
+                    System.out.println("CRIAR BIKE");
+                } else if (option == 2) {
+                    System.out.println("LISTAR TODAS AS BIKES");
+                } else if (option == 3) {
+                    System.out.println("LISTAR UMA BIKE POR ID");
+                } else if (option == 4) {
+                    System.out.println("ATUALIZAR TODAS AS INFOMAÇÔES DE UMA BIKE");
+                } else if (option == 5) {
+                    System.out.println("ATUALIZAR UMA INFORMAÇÂO ESPECIFICA DE UMA BIKE");
+                } else if (option == 6) {
+                    System.out.println("DELETAR UMA BIKE");
+                } else if (option == 7) {
+                    System.out.println("SAIR");
+                    System.out.println("ENCERRANDO");
+                    parada = 1;
+                }
+            }while (option != 1 && option != 2 && option != 3 &&
+                    option != 4 && option != 5 && option != 6 && option != 7);
+        }
     }
 }
