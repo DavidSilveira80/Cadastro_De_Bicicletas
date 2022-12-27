f1b6b3eaa347f9866cd5dcaa6f66bf0a467e8b8a
DavidSilveira80 <davidpscontato@gmail.com>
2022-12-26 23:28:44 -0300

    Iniciada primeira estrutura de opção do menu principal.

--- src/Aplicacao.java
+++ src/Aplicacao.java
@@ -1,8 +1,16 @@
 import menus.Menus;
-
+import java.util.Scanner;
 public class Aplicacao {
     public static void main(String[] args){
-        Menus.menuPrincipal();
+        Scanner entrada = new Scanner(System.in);
+
+        int option;
+        do{
+            Menus.menuPrincipal();
+            System.out.print("Informe sua escolha: ");
+            option = entrada.nextInt();
+        }while(option != 1 && option != 2 && option != 3 &&
+        option != 4 && option != 5 && option != 6 && option !=7);
 
     }
 }
